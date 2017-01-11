161bade44dcd853fc58b96ae73061c21dfed43c6
ΚΟΥΒΑΡΗΣ ΘΕΟΦΑΝΗΣ (E40496) <kouvaris.th@nbg.gr>
2017-01-11 12:33:37 +0200

    fix unit tests

--- StoreSimpleData/Api/StoreSimpleData.Tests/UnitTest1.cs
+++ StoreSimpleData/Api/StoreSimpleData.Tests/UnitTest1.cs
@@ -2,48 +2,52 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Data.ExternalStorage;
 using System.Linq;
-
+using System.Configuration;
 
 namespace StoreSimpleData.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
         public void ConnectToDatabase()
         {
-            IDb db = new Db("");
-            MindName input = new MindName() { Id = 4, Name = null };
-            var a = db.DeleteMindName(input);
+            string dbcon = ConfigurationManager.AppSettings["Dbcon"];
+            IDb db = new Db(dbcon);
+            MindTarget input = new MindTarget() { Id = 4, Target = null };
+            var a = db.DeleteMindTarget(input);
             Console.Write("Time:"+ DateTime.Now + " - Deleted:" + a);
-             input = new MindName() { Id = 4, Name = "Connection test 2" };
-            var b = db.InsertMindName(input);
+             input = new MindTarget() { Id = 4, Target = "Connection test 2" };
+            var b = db.InsertMindTarget(input);
             Console.Write("\nTime:" + DateTime.Now + " - Inserted:" + b);
         }
         [TestMethod]
         public void SelectPrintFirstDatabase()
         {
-            IDb db = new Db("");
+            string dbcon = ConfigurationManager.AppSettings["Dbcon"];
+            IDb db = new Db(dbcon);
             object input = new  { Id = 4 };
-            var a = db.SelectMindName().FirstOrDefault();
-            Console.Write("\nTime:" + DateTime.Now + " - Select:" + a.Id + " " + a.Name);
-            var b= db.SelectMindName(input).FirstOrDefault();
-            Console.Write("\nTime:" + DateTime.Now + " - Select:" + b.Id + " " + b.Name);
+            var a = db.SelectMindTarget().FirstOrDefault();
+            Console.Write("\nTime:" + DateTime.Now + " - Select:" + a.Id + " " + a.Target);
+            var b= db.SelectMindTarget(input).FirstOrDefault();
+            Console.Write("\nTime:" + DateTime.Now + " - Select:" + b.Id + " " + b.Target);
         }
         [TestMethod]
         public void UpdateFirstDatabase()
         {
-            IDb db = new Db("");
-            object input = new { Id = 4, Name = "For Update" };
-            var b2 = db.UpdateMindName(input);
+            string dbcon = ConfigurationManager.AppSettings["Dbcon"];
+            IDb db = new Db(dbcon);
+            object input = new { Id = 4, Target = "For Update" };
+            var b2 = db.UpdateMindTarget(input);
             Console.Write("\nTime:" + DateTime.Now + " - Update count:" + b2);
-            var a = db.SelectMindName(input).FirstOrDefault();
-            input = new { Id = 4, Name = "Updated" };
-            Console.Write("\nTime:" + DateTime.Now + " - Select:" + a.Id + " " + a.Name);
-            var b = db.UpdateMindName(input);
+            var a = db.SelectMindTarget(input).FirstOrDefault();
+            input = new { Id = 4, Target = "Updated" };
+            Console.Write("\nTime:" + DateTime.Now + " - Select:" + a.Id + " " + a.Target);
+            var b = db.UpdateMindTarget(input);
             Console.Write("\nTime:" + DateTime.Now + " - Update count:" + b );
-            var c = db.SelectMindName(input).FirstOrDefault();
-            Console.Write("\nTime:" + DateTime.Now + " - Select:" + c.Id + " " + c.Name);
+            var c = db.SelectMindTarget(input).FirstOrDefault();
+            Console.Write("\nTime:" + DateTime.Now + " - Select:" + c.Id + " " + c.Target);
         }
     }
 }
